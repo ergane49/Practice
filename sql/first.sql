dc75a07d380c33103b827fda294cc3da1c131a28
ergane49 <ergane49@gmail.com>
2025-09-06 01:22:31 +0900

    Create first.sql

---
+++ sql/first.sql
@@ -0,0 +1,13 @@
+<h1>Hello from my first web page</h1>
+
+<?php
+echo "<p>Hi there<br>";
+$answer = 6 * 7;
+echo "The answer is {$answer} — what was the question</p>";
+?>
+
+<p><span class="muted">Another paragraph</span></p>
+
+<style>
+  .muted { color: #ffa6a6; }
+</style>
